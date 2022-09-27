7647423301c81a53c524a6f7ba705bce575f70b6
ankush-003 <anshhv2003@gmail.com>
2022-09-28 01:12:29 +0530

    modified dll.c

--- Problems/music-streaming system/dll.c
+++ Problems/music-streaming system/dll.c
@@ -14,30 +14,134 @@ list_t* create_list()  // return a newly created empty doubly linked list
 
 void insert_front(list_t* list, int data)  // TODO: inserts data to the beginning of the linked list
 {
+	node_t *newNode = (node_t *) malloc(sizeof(node_t));
+	newNode->data = data;
+	newNode->next = NULL;
+	newNode->prev = NULL;
+	if(is_empty(list))
+	{
+		list->head = list->tail = newNode;
+	}
+	else {
+		newNode->next = list->head;
+		list->head->prev = newNode;
+		list->head = newNode;
+	}
+	list->size++;
 }
 
 void insert_back(list_t* list, int data) // TODO: inserts data to the end of the linked list
 {
+	node_t *newNode = (node_t *) malloc(sizeof(node_t));
+	newNode->data = data;
+	newNode->next = NULL;
+	newNode->prev = NULL;
+	if(is_empty(list))
+	{
+		list->head = list->tail = newNode;
+	}
+	else {
+		newNode->prev = list->tail;
+		list->tail->next = newNode;
+		list->tail = newNode;
+	}
+	list->size++;
 }
 
 void insert_after(list_t* list, int data, int prev) // TODO: inserts data after the node with data “prev”. Do not insert or do anything if prev doesn't exist
 {
+	if(is_empty(list)) {
+		// printf("List is Empty!\n");
+		return;
+	}
+	else {
+		node_t *temp = list->head;
+		node_t *newNode = (node_t *) malloc(sizeof(node_t));
+		newNode->data = data;
+		newNode->next = NULL;
+		newNode->prev = NULL;
+		// Traversing list till prev is found in list
+		while ((temp->data != prev)&&(temp != NULL))
+		{
+			temp = temp->next;
+		}
+		if(temp->data == prev) {
+			if(temp == list->tail)
+			{
+				temp->next = newNode;
+				newNode->prev = temp;
+				list->tail = newNode;
+			}
+			else {
+				newNode->next = temp->next;
+				temp->next->prev = newNode;
+				newNode->prev = temp;
+				temp->next = newNode;
+			}
+			list->size++;
+		}
+	}
 }
 
 void delete_front(list_t* list) // TODO: delete the start node from the linked list.
 {
+	if(is_empty(list))
+		return;
+
+	node_t *temp = list->head;
+	temp->next->prev = NULL;
+	list->head = temp->next;
+	list->size--;
+	free(temp);
+	temp = NULL;	
 }
 
 void delete_back(list_t* list) // TODO: delete the end node from the linked list.
 {
+	if(is_empty(list))
+		return;
+
+	node_t *temp = list->tail;
+	temp->prev->next = NULL;
+	list->tail = temp->prev;
+	list->size--;
+	free(temp);
+	temp = NULL;
 }
 
 void delete_node(list_t* list, int data) // TODO: delete the node with “data” from the linked list.
 {
+	if(is_empty(list))
+		return;
+
+	node_t *temp = list->head;
+	while((temp->data != data)&&(temp != NULL))
+		temp = temp->next;
+
+	if(temp->data == data) {
+		if(temp == list->head)
+			delete_front(list);
+		else if (temp == list->tail)
+			delete_back(list);
+		else {
+			temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			free(temp);
+			temp = NULL;
+			list->size--;
+		}	
+	}	
 }
 
 node_t* search(list_t* list, int data) // TODO: returns the pointer to the node with “data” field. Return NULL if not found.
 {	
+	if(is_empty(list))
+		return NULL;
+
+	node_t *cur = list->head;
+	while((cur->data != data)&&(cur != NULL))
+		cur = cur->next;
+	return cur;
 }
 
 int is_empty(list_t* list) // return true or 1 if the list is empty; else returns false or 0
