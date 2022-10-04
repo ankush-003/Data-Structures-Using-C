212b558cd4069bf1fb0db42d5859ca741fb851de
ankush-003 <anshhv2003@gmail.com>
2022-10-04 20:23:23 +0530

    created Expression Tree

---
+++ Unit 3/class/expTree.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <math.h>
+
+struct expressionTree {
+    char op;
+    struct expressionTree *lchild;
+    struct expressionTree *rchild;
+};
+typedef struct expressionTree ETree;
+
+struct stack {
+    ETree *ele[20];
+    int top;
+};
+typedef struct stack S;
+
+int isOperator(char e) {
+    switch(e) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '^':
+            return 1;
+        default:
+            return 0;    
+    }
+}
+
+void initS(S *sptr) {
+
+}
+
+ETree *createNode(char e) {
+    ETree *newNode = (ETree *) malloc(sizeof(ETree));
+    newNode->op = e;
+    newNode->lchild = NULL;
+    newNode->rchild = NULL;
+    return newNode;
+}
+
+ETree *buildETree(ETree *root,char *postFix) {
+    int len = strlen(postFix);
+    S auxS;
+    initS(&auxS);
+    ETree *newNode;
+    for(int i = 0;i < len;i++) {
+        if(isOperator(postFix[i])) {
+            ETree *rop,*lop;
+            rop = pop(&auxS);
+            lop = pop(&auxS);
+            newNode = createNode(postFix[i]);
+            newNode->lchild = lop;
+            newNode->rchild = rop;
+            push(&auxS,newNode);
+        }
+        else {// is an operand
+            newNode = createNode(postFix[i]);
+            push(&auxS,newNode);
+        }
+    }
+    return pop(&auxS);
+}
+
+float evaluateOp(ETree *root){
+    switch(root->op) {
+        case '+':
+            return;
+        case '-':
+        case '*':
+        case '/':
+        case '^':
+            return 1;
+        default:
+            return 0;    
+    }
+}
+
+float evalETree(ETree *root) {
+    if(isOperator(root->op)) {
+        return evaluateOp(root);
+    }
+}
+
+int main()
+{
+    char postFix[30];
+    ETree *root = NULL;
+    printf("\nEnter the postfix Expression: ");
+    scanf("%s",postFix);
+    root = buildETree(root,postFix);
+    float result = -1;
+    result = evalETree(root);
+    return 0;
+}
