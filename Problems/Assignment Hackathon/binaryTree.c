7c9593f62fc7ff5bc69e4503e06f3254b9292c6d
ankush-003 <anshhv2003@gmail.com>
2022-11-15 11:03:23 +0530

    completed binaryTree

--- Problems/Assignment Hackathon/binaryTree.c
+++ Problems/Assignment Hackathon/binaryTree.c
@@ -12,33 +12,58 @@ struct treeNode {
 };
 typedef struct treeNode treeNode;
 
-void createNode(treeNode **node, int data) {
-    if(*node == NULL) {
-        *node = (treeNode *) malloc(sizeof(node));
-        (*node)->data = data;
-        (*node)->left = NULL;
-        (*node)->right = NULL;
-    }
-    printf("Enter left child of %d(-1 to end): ", data);
-    scanf("%d", &data);
-    if(data != -1) {
-        createNode(&(*node)->left, data);
-    }
-    printf("Enter right child of %d(-1 to end): ", data);
-    scanf("%d", &data);
-    if(data != -1) {
-        createNode(&(*node)->right, data);
-    }
+void createNode(treeNode **node) {
+    treeNode *newNode = (treeNode *) malloc(sizeof(treeNode));
+    int info;
+    printf("Enter Node info(-1 to end): ");
+    scanf("%d",&info);
+    
+    if(info == -1)
+        *node = NULL;
+    else {
+        *node = newNode;
+        (*node)->data = info;
+        printf("Creating left child of %d\n",info);
+        createNode(&(*node)->left);
+        printf("Creating right child of %d\n",info);
+        createNode(&(*node)->right);
+    }    
 }
 
-void inorder(node *root) {
+void inorder(treeNode*root) {
     if(root == NULL) {
-        retun;
+        return;
     }
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+//counting height of tree
+int height(treeNode *root) {
+    if(root == NULL) {
+        return 0;
+    }
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    return (leftHeight > rightHeight) ? leftHeight +1 : rightHeight+1;
+}
+
+int numberOfNodes(treeNode *root) {
+    if(root == NULL) {
+        return 0;
+    }
+    return numberOfNodes(root->left) + numberOfNodes(root->right) + 1;
 }
 
 int main()
 {
-    // Code
+    treeNode *root = NULL;
+    int data;
+    printf("Creating root\n");
+    createNode(&root);
+    inorder(root);
+    printf("\nHeight of tree: %d\n",height(root) - 1);// if root is taken as 0
+    printf("Number of nodes: %d\n",numberOfNodes(root));
     return 0;
 }
